ae176baf45802b2eb3cd4b457bb9d9d0c0260988
Justin <JEStaubach@users.noreply.github.com>
2021-06-26 14:50:29 -0400

    pull tf stuff out to ou

--- locals.tf
+++ locals.tf
@@ -10,5 +10,10 @@ data "terraform_remote_state" "terraform-devl" {
 
 locals {
   #project_number = data.terraform_remote_state.terraform-devl.outputs.projects
-  project_number = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.number if x.path == "dev/xascode/devl/corp/randd/snapshots/snapshots"][0]
+  project_id = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.id if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]
+  project_name = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.name if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]
+  project_number = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.number if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]
+  project_path = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.path if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]
+  project_dot_path = join(".",split("/", [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.number if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]))
+  project_dash_path = join("-",split("/", [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x.number if x.path == "dev/xascode/devl/corp/snapshots/snapshots"][0]))
 }
