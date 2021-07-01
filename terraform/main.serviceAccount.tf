32d22b1215460d1aa1265f521f77bda66718461e
Justin <JEStaubach@users.noreply.github.com>
2021-07-01 19:45:58 -0400

    apply at parent rather than org

--- terraform/main.serviceAccount.tf
+++ terraform/main.serviceAccount.tf
@@ -38,8 +38,8 @@ resource "google_service_account" "svc-check-snapshots" {
   project      = local.project.id
 }
 
-resource "google_organization_iam_binding" "organization" {
-  org_id  = var.organization_id
+resource "google_folder_iam_binding" "parent_folder" {
+  folder  = local.parent.name
   role    = google_organization_iam_custom_role.role-svc-check-snapshots.name
   members = [
     "serviceAccount:${google_service_account.svc-check-snapshots.email}",
