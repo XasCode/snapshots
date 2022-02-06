1f41a8bf7c6841d6a2b4806044d4de33d31319fa
Justin <JEStaubach@users.noreply.github.com>
2022-02-05 23:29:42 -0500

    comment out so can modify bucket withou recreating everything.

--- terraform/main.buckets.tf
+++ terraform/main.buckets.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "bucket" {
-  name = "snapshots-bucket-${random_id.random.hex}"
-  project = local.project.id
-}
+//resource "google_storage_bucket" "bucket" {
+//  name = "snapshots-bucket-${random_id.random.hex}"
+//  project = local.project.id
+//}
 
 resource "google_storage_bucket" "backup_records" {
   name = "backup_records_${local.project.id}"
@@ -9,8 +9,8 @@ resource "google_storage_bucket" "backup_records" {
   force_destroy = true
 }
 
-resource "google_storage_bucket_object" "archive" {
-  name   = "index-${filemd5(data.archive_file.srcfiles.output_path)}.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = data.archive_file.srcfiles.output_path
-}
+//resource "google_storage_bucket_object" "archive" {
+//  name   = "index-${filemd5(data.archive_file.srcfiles.output_path)}.zip"
+//  bucket = google_storage_bucket.bucket.name
+//  source = data.archive_file.srcfiles.output_path
+/}
