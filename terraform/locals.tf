e085d0b07d52654d363c359f4368a2addf4faca5
Justin <JEStaubach@users.noreply.github.com>
2022-02-05 22:13:00 -0500

    remove extra folder

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -10,5 +10,5 @@ data "terraform_remote_state" "terraform-devl" {
 
 locals {
   project = [for x in data.terraform_remote_state.terraform-devl.outputs.projects: x if x.path == var.project_path][0]
-  parent = [for x in data.terraform_remote_state.terraform-devl.outputs.folders: x if x.path == "${join("/", slice(split("/", var.project_path), 0, length(split("/", var.project_path))-2))}/"][0]
+  parent = [for x in data.terraform_remote_state.terraform-devl.outputs.folders: x if x.path == "${join("/", slice(split("/", var.project_path), 0, length(split("/", var.project_path))-1))}/"][0]
 }
